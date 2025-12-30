c272feac08cd98486868d93b3cbda4fab00b7871
Branko420 <kolevb780@gmail.com>
2025-12-30 18:44:16 +0200

    debug

--- lib/robot_arm_ui/pi_client.ex
+++ lib/robot_arm_ui/pi_client.ex
@@ -6,7 +6,7 @@ defmodule RobotArmUi.PiClient do
     WebSockex.start_link(url, __MODULE__, %{}, name: __MODULE__)
   end
 
-  def send_frame(frame) when is_binary(payload) do
+  def send_frame(payload) when is_binary(payload) do
     WebSockex.send_frame(__MODULE__, {:text, payload})
   end
 
@@ -15,8 +15,8 @@ defmodule RobotArmUi.PiClient do
     {:ok, state}
   end
 
-  def handle_disconnect(_conn, state) do
-    Logger.info("PiClient: Disconnected from Raspberry. Reason: #{inspect(conn_status.reason)}")
+  def handle_disconnect(conn, state) do
+    Logger.info("PiClient: Disconnected from Raspberry. Reason: #{inspect(conn.reason)}")
     {:ok, state}
   end
 end
